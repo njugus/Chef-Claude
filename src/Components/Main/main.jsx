7bcb9d4a46eff5486633f671e83795215e981bbf
njugus <kayvohnjuguna@gmail.com>
2025-01-07 14:33:15 -0800

    adds some responsiveness to the main section

--- src/Components/Main/main.jsx
+++ src/Components/Main/main.jsx
@@ -3,12 +3,15 @@ import "./main.css"
 const Main = () => {
     return (
         <>
-        <main className = "main-section">
+        <main className="main-section">
+        <form className = "form-section">
             <input  aria-label="add ingredient"  type = "text" placeholder="e.g tomato paste" 
             />
             <button>Add ingredient</button>
+        </form>
         </main>
         </>
+       
     )
 }
 
